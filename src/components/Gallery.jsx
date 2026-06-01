094dfeac20a2ab207e9085fa151af5fdf1039b50
Ji <seung07181@gmail.com>
2026-06-01 22:26:11 +0900

    사진 조정

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -39,7 +39,7 @@ import img26 from '../assets/gallery/photo26.jpg';
 
 // 💡 여기서 순서 정하기
 const images = [
-    img1, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13, img14, img15, img16, img17, img18, img19, img20, img21, img22, img23, img24, img25, img26,
+    img1, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13, img16, img17, img18, img19, img20, img21, img22, img23, img24, img25, img26,
 ];
 
 export default function Gallery() {
